113d462f5e3a3b25fe91f0685960c6618beb030d
Łukasz Wasiluk <galawanty@gmail.com>
2017-03-02 22:23:15 +0100

    zadanie 4

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+
+// a = 5
+// b = 6
+
+var a = 5
+var b = 6
+var value = (a*a) + (2 * a * b) - (b*b)
+
+document.write(value)
+
+if (value > 0) {
+		console.log('wynik dodatni');
+} else {
+		console.log('wynik ujemny');
+}
+
